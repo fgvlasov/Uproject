e5c89e57090244c9d4f2f464815c4d44ba8575ff
fgvlasov <fgvlasov@gmail.com>
2022-10-05 08:42:19 +0400

    fixed some errors of linter 3

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
+import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
-    renderWithTranslation
+    renderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
-import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
-    test('Test render', () => {
+    test('with only first param', () => {
         renderWithTranslation(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
-    test('Test toggle', () => {
+
+    test('test toggle', () => {
         renderWithTranslation(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
